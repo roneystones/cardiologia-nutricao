ce04e351afd6f1485243f372575a115909d1cd0d
Roney Stones <roneylacerda@gmail.com>
2018-04-02 15:47:17 -0300

    design home e quem somos

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,6 @@ gulp.task('build', shell.task(['bundle exec jekyll serve']));
 // Task for serving blog with Browsersync
 gulp.task('serve', function () {
     browserSync.init({server: {baseDir: '_site/'}});
-    // Reloads page when some of the already built files changed:
     gulp.watch('_site/**/*.*').on('change', browserSync.reload);
 });
 
